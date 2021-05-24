3a88ec309c161eb21d015114964f79d7b98cb096
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-05-24 10:22:00 +0100

    Added initial domain model with NationalPark object

--- ParkManagement.Domain/Models/NationalPark.cs
+++ ParkManagement.Domain/Models/NationalPark.cs
@@ -1,14 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using ParkManagement.Domain.Validators;
 
 namespace ParkManagement.Domain.Models
 {
     public class NationalPark
     {
+        [CurrentDateLimit(ErrorMessage = "The date created cannot be later than today")]
+        public DateTimeOffset? DateCreated { get; set; }
+
+        [CurrentDateLimit(ErrorMessage = "The date established cannot be later than today")]
+        public DateTimeOffset? DateEstablished { get; set; }
+
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
         public string Name { get; set; }
-        public DateTimeOffset DateCreated { get; set; }
-        public DateTimeOffset DateEstablished { get; set; }
+        public byte[] Picture { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string State { get; set; }
     }
 }
