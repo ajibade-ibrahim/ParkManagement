95f00cee794672d9a00dc4738fdd2665990c304e
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-05-24 08:30:42 +0100

    Add project files.

---
+++ ParkManagement.Domain/Models/NationalPark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkManagement.Domain.Models
+{
+    public class NationalPark
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset DateEstablished { get; set; }
+    }
+}
