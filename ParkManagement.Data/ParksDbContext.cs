a7f1df65034c7af26c861917f5ebd3b63ce1d368
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-05-24 15:32:41 +0100

    Cleaned File

--- ParkManagement.Data/ParksDbContext.cs
+++ ParkManagement.Data/ParksDbContext.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
-using ParkManagement.Domain.Models;
+using ParkManagement.Domain.Models.Entities;
 
 namespace ParkManagement.Data
 {
