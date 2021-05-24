f11eaa8ab131b3ef68ee2205a17994defe54e1bf
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-05-24 10:21:36 +0100

    Added data project with first migration

---
+++ ParkManagement.Data/Migrations/20210524091059_Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkManagement.Data.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NationalParks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    DateEstablished = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Picture = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NationalParks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NationalParks");
+        }
+    }
+}
---
+++ ParkManagement.Data/ParksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using ParkManagement.Domain.Models;
+
+namespace ParkManagement.Data
+{
+    public class ParksDbContext : DbContext
+    {
+        public ParksDbContext([NotNull] DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
