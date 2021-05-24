344aff7f2b9673b8b39dc39a0fa20a2d599a824d
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2021-05-24 09:40:31 +0100

    Added validator to ensure entered dates aren't greater than current date

---
+++ ParkManagement.Domain/Validators/CurrentDateLimitAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkManagement.Domain.Validators
+{
+    public sealed class CurrentDateLimitAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            return value == null || ((DateTimeOffset)value).Date <= DateTimeOffset.Now.Date;
+        }
+    }
+}
